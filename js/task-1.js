df29eef0495fed7697248358cb218cbabbe57165
Leon <leon.filhitsayh@gmail.com>
2024-02-23 17:27:27 +0200

    Update task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,6 @@
 function makeTransaction (quantity,pricePerDroid) {
-    return quantity*pricePerDroid;
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
 }
 
 //    Result
